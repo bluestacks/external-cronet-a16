97888eb90662d196abf3e77d3438b0af40b89ada
Cronet Mainline Eng <cronet-mainline-eng+copybara@google.com>
2025-05-09 06:51:45 +0000

    Import Cronet e158082a (138.0.7171.0) into tot
    Chromium commit hash: e158082ac4d1a7f8907cf6a083626bd648bf8e23
    Chromium commit date: 2025-05-08 23:20:10 -0700
    Chromium version: 138.0.7171.0
    
    This CL was autogenerated by the following Chromium bot run:
    https://luci-milo.appspot.com/swarming/task/70cc9db7d1269611?server=chrome-swarming.appspot.com
    
    This CL can be reproduced by running the following command:
    gclient config --spec 'solutions = [
    {
        "name": "src",
        "url": "https://chromium.googlesource.com/chromium/src.git",
        "managed": False,
        "custom_deps": {},
        "custom_vars": {
          "checkout_copybara": True,
        },
      },
    ]
    target_os = ["android"]
    ' && gclient sync --rev=e158082ac4d1a7f8907cf6a083626bd648bf8e23 && cd src
    && vpython3 components/cronet/gn2bp/run_gn2bp.py --channel=tot
    
    The state of Chromium, for the commit being imported, can be browsed at:
    https://chromium.googlesource.com/chromium/src/+/e158082ac4d1a7f8907cf6a083626bd648bf8e23
    
    FolderOrigin-RevId: /b/s/w/ir/cache/builder/src
    Change-Id: Id0022abe09eff681174b75d278d11be246cbacec

--- tot/testing/scripts/run_performance_tests.py
+++ tot/testing/scripts/run_performance_tests.py
@@ -31,7 +31,7 @@ could be histogram or graph json), and test_results.json.
 TESTING:
 To test changes to this script, please run unit tests:
 $ cd testing/scripts
-$ python3 -m unittest run_performance_tests_unittest.py
+$ vpython3 run_performance_tests_unittest.py
 
 Run end-to-end tests:
 $ cd tools/perf
@@ -739,6 +739,7 @@ class CrossbenchTest(object):
 
   def __init__(self, options, isolated_out_dir):
     self.options = options
+    self._parse_arguments()
     self.isolated_out_dir = isolated_out_dir
     self.network = self._get_network_arg(options.passthrough_args)
     if self.options.luci_chromium:
@@ -751,7 +752,15 @@ class CrossbenchTest(object):
       browser_arg = _get_browser_arg(options.passthrough_args)
       self.is_android = _is_android(browser_arg)
       self._find_browser(browser_arg)
-      self.driver_path_arg = self._find_chromedriver()
+
+  def _parse_arguments(self):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--official-browser',
+                        type=str,
+                        required=False,
+                        help='Use official build of the browser')
+    self.cb_options, self.options.passthrough_args = parser.parse_known_args(
+        self.options.passthrough_args)
 
   def _get_network_arg(self, args):
     if _arg := _get_arg(args, '--network='):
@@ -803,12 +812,29 @@ class CrossbenchTest(object):
       self.options.passthrough_args.remove(wpr_arg)
     return [_create_network_json('wpr', path=archive, wpr_go_bin=wpr_go)]
 
+  def _check_for_embedder_arg(self):
+    embedder_arg = _get_arg(self.options.passthrough_args, '--embedder=')
+    if embedder_arg:
+      embedder_package_name = embedder_arg.split('=', 1)[1]
+      # This will affect browser arg, but is not to be passed by itself
+      self.options.passthrough_args.remove(embedder_arg)
+      return embedder_package_name
+    return None
+
   def _find_browser(self, browser_arg):
     # Replacing --browser with the generated self.browser.
     self.options.passthrough_args = [
         arg for arg in self.options.passthrough_args
         if not arg.startswith('--browser=')
     ]
+    if self.cb_options.official_browser:
+      if self.is_android:
+        raise RuntimeError(
+            'Running official build not yet supported on Android')
+      self.browser = self.CHROME_BROWSER % self.cb_options.official_browser
+      self.driver_path_arg = []
+      return
+    self.driver_path_arg = self._find_chromedriver()
     if '/' in browser_arg or '\\' in browser_arg:
       # The --browser arg looks like a path. Use it as-is.
       self.browser = self.CHROME_BROWSER % browser_arg
@@ -821,7 +847,9 @@ class CrossbenchTest(object):
     if not possible_browser:
       raise ValueError(f'Unable to find Chrome browser of type: {browser_arg}')
     if self.is_android:
-      browser_app = possible_browser.settings.package
+      # Check for an arg with embedder package name to override browser (WV)
+      browser_app = (self._check_for_embedder_arg()
+                     or possible_browser.settings.package)
       android_json = self.ANDROID_HJSON % (browser_app, ADB_TOOL)
       self.browser = self.CHROME_BROWSER % android_json
     else:

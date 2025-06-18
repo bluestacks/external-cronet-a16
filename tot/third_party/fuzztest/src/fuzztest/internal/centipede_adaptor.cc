88a7bc609675533b79f44daf3e117107e09c0f6c
Cronet Mainline Eng <cronet-mainline-eng+copybara@google.com>
2025-06-18 10:14:28 +0000

    Import Cronet 1293b4d4 (139.0.7247.0) into tot
    Chromium commit hash: 1293b4d44cd8c42761ba931aac96c882f663e4fc
    Chromium commit date: 2025-06-18 02:57:36 -0700
    Chromium version: 139.0.7247.0
    
    This CL was autogenerated by the following Chromium bot run:
    https://luci-milo.appspot.com/swarming/task/719b4c15c224d811?server=chrome-swarming.appspot.com
    
    This CL can be reproduced by running the following command:
    gclient config --spec 'solutions = [
    {
        "name": "src",
        "url": "https://chromium.googlesource.com/chromium/src.git",
        "managed": False,
        "custom_deps": {},
        "custom_vars": {
          "checkout_copybara": True,
        },
      },
    ]
    target_os = ["android"]
    ' && gclient sync --rev=1293b4d44cd8c42761ba931aac96c882f663e4fc && cd src
    && vpython3 components/cronet/gn2bp/run_gn2bp.py --channel=tot
    
    The state of Chromium, for the commit being imported, can be browsed at:
    https://chromium.googlesource.com/chromium/src/+/1293b4d44cd8c42761ba931aac96c882f663e4fc
    
    FolderOrigin-RevId: /b/s/w/ir/cache/builder/src
    Change-Id: I9fd9cedc1609dd3da0f192abb5f1bf9f2bac8235

--- tot/third_party/fuzztest/src/fuzztest/internal/centipede_adaptor.cc
+++ tot/third_party/fuzztest/src/fuzztest/internal/centipede_adaptor.cc
@@ -27,6 +27,7 @@
 #include <algorithm>
 #include <cerrno>
 #include <cinttypes>
+#include <csignal>
 #include <cstddef>
 #include <cstdint>
 #include <cstdio>
@@ -71,6 +72,7 @@
 #include "./centipede/mutation_input.h"
 #include "./centipede/runner_interface.h"
 #include "./centipede/runner_result.h"
+#include "./centipede/stop.h"
 #include "./centipede/workdir.h"
 #include "./common/defs.h"
 #include "./common/remote_file.h"
@@ -330,8 +332,35 @@ fuzztest::internal::Environment CreateCentipedeEnvironmentFromConfiguration(
   return env;
 }
 
+void InstallCentipedeTerminationHandler() {
+  [[maybe_unused]] static bool install_once = [] {
+    for (int signum : {SIGTERM, SIGHUP}) {
+      struct sigaction new_sigact = {};
+      sigemptyset(&new_sigact.sa_mask);
+      new_sigact.sa_handler = [](int unused_signum) {
+        Runtime::instance().SetTerminationRequested();
+        RequestEarlyStop(EXIT_FAILURE);
+      };
+
+      // We make use of the SA_ONSTACK flag so that signal handlers are
+      // executed on a separate stack. This is needed to properly handle
+      // cases where stack space is limited and the delivery of a signal
+      // needs to be properly handled.
+      new_sigact.sa_flags = SA_ONSTACK;
+
+      FUZZTEST_INTERNAL_CHECK(sigaction(signum, &new_sigact, nullptr) == 0,
+                              "Error installing signal handler: %s\n",
+                              strerror(errno));
+    }
+    return true;
+  }();
+}
+
 int RunCentipede(const Environment& env,
                  const std::optional<std::string>& centipede_command) {
+  if (Runtime::instance().termination_requested()) {
+    return EXIT_FAILURE;
+  }
   if (centipede_command.has_value()) {
     std::string cmdline = "exec 2>&1 ";
     absl::StrAppend(&cmdline, *centipede_command);
@@ -342,7 +371,7 @@ int RunCentipede(const Environment& env,
     absl::FPrintF(GetStderr(), "[.] Running Centipede command %s\n", cmdline);
     const std::vector<std::string> shell_cmd = {"/bin/sh", "-c",
                                                 std::move(cmdline)};
-    const TerminationStatus status = RunCommandWithOutputCallbacks(
+    const TerminationStatus status = RunCommandWithCallbacks(
         shell_cmd,
         [](absl::string_view stdout_output) {
           std::fwrite(stdout_output.data(), 1, stdout_output.size(),
@@ -352,6 +381,7 @@ int RunCentipede(const Environment& env,
           std::fwrite(stderr_output.data(), 1, stderr_output.size(),
                       GetStderr());
         },
+        [] { return Runtime::instance().termination_requested(); },
         /*environment=*/std::nullopt);
     if (status.Signaled()) {
       // Encoding signaled exit similarly as Bash.
@@ -760,17 +790,18 @@ absl::Status ExportReproducersFromCentipede(
                     absl::StrCat(read_reproducer_status));
       continue;
     }
-    const std::string metadata_file = std::filesystem::path{exported_crash_file}
-                                          .replace_extension("metadata")
-                                          .string();
-    std::string metadata;
-    const absl::Status read_metadata_status =
-        RemoteFileGetContents(metadata_file, metadata);
-    if (!read_metadata_status.ok()) {
+    const std::string description_file =
+        std::filesystem::path{exported_crash_file}
+            .replace_extension("desc")
+            .string();
+    std::string description;
+    const absl::Status read_description_status =
+        RemoteFileGetContents(description_file, description);
+    if (!read_description_status.ok()) {
       absl::FPrintF(
           GetStderr(),
-          "[!] Got error while reading the metadata for crash id %s: %s\n",
-          crash_id, absl::StrCat(read_metadata_status));
+          "[!] Got error while reading the description for crash id %s: %s\n",
+          crash_id, absl::StrCat(read_description_status));
       continue;
     }
     std::string reproducer_path = WriteDataToDir(reproducer, output.dir_path);
@@ -782,8 +813,8 @@ absl::Status ExportReproducersFromCentipede(
       continue;
     }
     absl::FPrintF(GetStderr(),
-                  "[.] Saved reproducer with ID %s and crash metadata %s\n",
-                  Basename(reproducer_path), metadata);
+                  "[.] Saved reproducer with ID %s and crash description %s\n",
+                  Basename(reproducer_path), description);
     if (!single_reproducer_path.has_value()) {
       single_reproducer_path = reproducer_path;
     } else {
@@ -845,6 +876,8 @@ bool CentipedeFuzzerAdaptor::Run(int* argc, char*** argv, RunMode mode,
     // TODO(b/393582695): Consider whether we need some kind of reporting
     // enabled in the controller mode to handle test setup failures.
     runtime_.EnableReporter(&fuzzer_impl_.stats_, [] { return absl::Now(); });
+  } else {
+    InstallCentipedeTerminationHandler();
   }
   if (runner_mode) {
     runtime_.RegisterCrashMetadataListener(

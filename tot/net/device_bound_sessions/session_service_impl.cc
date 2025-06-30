5f91222654cc06792296692f9aa45fa45d099372
Cronet Mainline Eng <cronet-mainline-eng+copybara@google.com>
2025-06-30 03:26:46 +0000

    Import Cronet e059dbcc (140.0.7270.0) into tot
    Chromium commit hash: e059dbcc642661f8156745a2146f0b3f9edf012d
    Chromium commit date: 2025-06-29 20:08:13 -0700
    Chromium version: 140.0.7270.0
    
    This CL was autogenerated by the following Chromium bot run:
    https://luci-milo.appspot.com/swarming/task/71d7accab4ea9711?server=chrome-swarming.appspot.com
    
    This CL can be reproduced by running the following command:
    gclient config --spec 'solutions = [
    {
        "name": "src",
        "url": "https://chromium.googlesource.com/chromium/src.git",
        "managed": False,
        "custom_deps": {},
        "custom_vars": {
          "checkout_copybara": True,
        },
      },
    ]
    target_os = ["android"]
    ' && gclient sync --rev=e059dbcc642661f8156745a2146f0b3f9edf012d && cd src
    && vpython3 components/cronet/gn2bp/run_gn2bp.py --channel=tot
    
    The state of Chromium, for the commit being imported, can be browsed at:
    https://chromium.googlesource.com/chromium/src/+/e059dbcc642661f8156745a2146f0b3f9edf012d
    
    FolderOrigin-RevId: /b/s/w/ir/cache/builder/src
    Change-Id: I07e32f3e6314c58fa22b8da40e534f9fc0e59b3b

--- tot/net/device_bound_sessions/session_service_impl.cc
+++ tot/net/device_bound_sessions/session_service_impl.cc
@@ -268,6 +268,8 @@ void SessionServiceImpl::DeferRequestForRefresh(
   it->second.emplace_back(request, std::move(callback));
 
   auto* session = GetSession(session_key);
+  CHECK(session, base::NotFatalUntil::M147);
+  // TODO(crbug.com/417770933): Remove this block.
   if (!session) {
     // If we can't find the session, clear the `session_key` in the map
     // and continue all related requests. We can call this a fatal error
@@ -521,20 +523,9 @@ SessionError::ErrorType SessionServiceImpl::OnRegistrationCompleteInternal(
     OnAccessCallback on_access_callback,
     base::expected<SessionParams, SessionError> params_or_error) {
   if (!params_or_error.has_value()) {
-    // There was a failure attempting to register. This registration
-    // request could be used to clean up an existing session if the
-    // server returned `"continue": false` in the JSON. In that case, we
-    // need to delete the session. In all other cases, we failed to
-    // create a new session, so there's nothing to clean up.
-    const SessionError& error = params_or_error.error();
-    if (error.type == SessionError::ErrorType::kServerRequestedTermination &&
-        error.session_id.has_value()) {
-      Session::Id session_id(*error.session_id);
-      DeleteSessionAndNotify(DeletionReason::kServerRequested,
-                             SessionKey{error.site, std::move(session_id)},
-                             on_access_callback);
-    }
-    return error.type;
+    // We failed to create a new session, so there's nothing to clean
+    // up.
+    return params_or_error.error().type;
   }
 
   const SessionParams& params = *params_or_error;
@@ -590,10 +581,7 @@ SessionError::ErrorType SessionServiceImpl::OnRefreshRequestCompletionInternal(
     UnblockDeferredRequests(session_key, RefreshResult::kRefreshed);
   } else if (const SessionError& error = params_or_error.error();
              error.IsFatal()) {
-    Session::Id session_to_terminate =
-        error.session_id ? Session::Id(*error.session_id) : session_key.id;
-    DeleteSessionAndNotify(DeletionReason::kRefreshFatalError,
-                           SessionKey{error.site, session_to_terminate},
+    DeleteSessionAndNotify(DeletionReason::kRefreshFatalError, session_key,
                            on_access_callback);
     UnblockDeferredRequests(session_key, RefreshResult::kFatalError);
   } else {

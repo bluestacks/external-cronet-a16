eee3a12f4839984af55cbf2cc0f787023ee164a2
Cronet Mainline Eng <cronet-mainline-eng+copybara@google.com>
2025-08-12 04:29:45 +0000

    Import Cronet 90d599af (141.0.7353.0) into tot
    Chromium commit hash: 90d599af20328f65263b5972ceaa5bb8019bccbe
    Chromium commit date: 2025-08-11 21:18:17 -0700
    Chromium version: 141.0.7353.0
    
    This CL was autogenerated by the following Chromium bot run:
    https://luci-milo.appspot.com/swarming/task/72b5526f00b01e11?server=chrome-swarming.appspot.com
    
    This CL can be reproduced by running the following command:
    gclient config --spec 'solutions = [
    {
        "name": "src",
        "url": "https://chromium.googlesource.com/chromium/src.git",
        "managed": False,
        "custom_deps": {},
        "custom_vars": {
          "checkout_copybara": True,
        },
      },
    ]
    target_os = ["android"]
    ' && gclient sync --rev=90d599af20328f65263b5972ceaa5bb8019bccbe && cd src
    && vpython3 components/cronet/gn2bp/run_gn2bp.py --channel=tot
    
    The state of Chromium, for the commit being imported, can be browsed at:
    https://chromium.googlesource.com/chromium/src/+/90d599af20328f65263b5972ceaa5bb8019bccbe
    
    NO_IFTTT=Imported from Chromium.
    FolderOrigin-RevId: /b/s/w/ir/cache/builder/src
    Change-Id: Iceec68e71beb56da3b22c99a104f4f61351c698c

--- tot/base/android/device_info.cc
+++ tot/base/android/device_info.cc
@@ -35,7 +35,6 @@ struct IDeviceInfo {
   bool isTv;
   // Available only on Android T+.
   int32_t vulkanDeqpLevel;
-  bool isXr;
 };
 #endif
 
@@ -56,7 +55,6 @@ IDeviceInfo& get_device_info() {
 
 void Set(const IDeviceInfo& info) {
   std::optional<IDeviceInfo>& holder = get_holder();
-  DCHECK(!holder.has_value());
   holder.emplace(info);
 }
 
@@ -66,15 +64,13 @@ static void JNI_DeviceInfo_FillFields(JNIEnv* env,
                                       jboolean isAutomotive,
                                       jboolean isFoldable,
                                       jboolean isDesktop,
-                                      jint vulkanDeqpLevel,
-                                      jboolean isXr) {
+                                      jint vulkanDeqpLevel) {
   Set(IDeviceInfo{.gmsVersionCode = gmsVersionCode,
                   .isAutomotive = static_cast<bool>(isAutomotive),
                   .isDesktop = static_cast<bool>(isDesktop),
                   .isFoldable = static_cast<bool>(isFoldable),
                   .isTv = static_cast<bool>(isTV),
-                  .vulkanDeqpLevel = vulkanDeqpLevel,
-                  .isXr = static_cast<bool>(isXr)});
+                  .vulkanDeqpLevel = vulkanDeqpLevel});
 }
 
 const std::string& gms_version_code() {
@@ -106,17 +102,4 @@ int32_t vulkan_deqp_level() {
   return get_device_info().vulkanDeqpLevel;
 }
 
-bool is_xr() {
-  return get_device_info().isXr;
-}
-
-void set_is_xr_for_testing() {
-  Java_DeviceInfo_setIsXrForTesting(AttachCurrentThread());  // IN-TEST
-  get_holder().reset();
-}
-
-void reset_is_xr_for_testing() {
-  Java_DeviceInfo_resetIsXrForTesting(AttachCurrentThread());  // IN-TEST
-  get_holder().reset();
-}
 }  // namespace base::android::device_info
